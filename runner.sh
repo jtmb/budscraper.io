4602c175d37cb3f32ab6f74694acda880c9c6dab
jtmb <james.branco@gmail.com>
2023-07-13 01:55:31 -0400

    updated runner

--- runner.sh
+++ runner.sh
@@ -2,12 +2,9 @@
 sudo docker compose --env-file nginx/.env -p "msg-converter" -f "./nginx/docker-compose.yml" up -d --build
 sudo docker exec msg-converter-php-1 sh /usr/local/bin/entrypoint
 
-# Show random generated db_pwd at the end of build
-db_pwd=$(cat ~/repos/msg-converter/web-app/nginx/.env | rev | cut -d "=" -f1 | rev)
-
 echo
 echo "-----------------------"
-echo db login info: 
-echo "user; root"
-echo "pwd; $db_pwd"
+echo "                       "
+echo "        RUNNING        "
+echo "                       "
 echo "-----------------------"
