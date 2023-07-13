ca82910f2e85a2a9b0627340dfc9becbe603a244
jtmb <james.branco@gmail.com>
2023-07-13 02:13:03 -0400

    updated runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,7 @@
 # Build docker compose stack and set entry point script
-sudo docker compose --env-file nginx/.env -p "msg-converter" -f "./nginx/docker-compose.yml" up -d --build
-sudo docker exec msg-converter-php-1 sh /usr/local/bin/entrypoint
+web_app_name=budscraper_app
+sudo docker compose --env-file nginx/.env -p "$web_app_name" -f "./nginx/docker-compose.yml" up -d --build
+sudo docker exec $web_app_name-php-1 sh /usr/local/bin/entrypoint
 
 echo
 echo "-----------------------"
